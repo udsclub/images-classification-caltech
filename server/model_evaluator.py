3f5b4f7a6909cff6b6170e56216e2adeca5b7d9c
Illarion Khlestov <illarion.khlestov@ring.com>
2017-09-07 23:14:59 +0300

    Update model evaluator

--- server/model_evaluator.py
+++ server/model_evaluator.py
@@ -22,8 +22,8 @@ results_db = redis.StrictRedis(
 
 
 ### Some settings
-# base_dir = '/Users/illarionkhliestov/Datasets/caltech-256/'
-base_dir = '/home/ubuntu/Datasets/caltech-256'
+base_dir = '/Users/illarionkhliestov/Datasets/caltech-256/'
+# base_dir = '/home/ubuntu/Datasets/caltech-256'
 csv_path = os.path.join(base_dir, 'kaggle_test.csv')
 df = pd.read_csv(csv_path)
 images_dir = os.path.join(base_dir, 'test')
@@ -32,26 +32,6 @@ public_images = df[df['Usage'] == 'Public']
 
 def gray_to_color(image):
     return np.repeat(image[:, :, np.newaxis], 3, axis=2)
-
-
-def get_images_and_targets():
-    x_test = []
-    x_test_resized = []
-    y_test = []
-    for idx, row in tqdm(public_images.iterrows(), total=public_images.shape[0], desc="Get images"):
-        image_path = os.path.join(images_dir, row['image'])
-        image = imread(image_path)
-        if len(image.shape) != 3:
-            image = gray_to_color(image)
-
-        x_test.append(np.expand_dims(image, axis=0) / 255)
-        image = imresize(image, (224, 224, 3))
-        x_test_resized.append(np.expand_dims(image, axis=0) / 255)
-        y_test.append(int(row['class'] - 1))
-    return x_test, x_test_resized, y_test
-
-
-x_test, x_test_resized, y_test = get_images_and_targets()
 
 
 def load_model_my(model_path):
@@ -89,21 +69,30 @@ def get_model_size(model):
 def get_model_accuracy(model):
     # get first predition to choose resolution of the images
     try:
-        model.predict(x_test[0])
-        test_images = x_test
+        model.predict(np.random.randn(1, 374, 530, 3))
+        resized = False
     except ValueError:
-        test_images = x_test_resized
+        resized = True
 
     start = time.time()
 
     correct = 0
-    for image, true_class in zip(test_images, y_test):
+    for idx, row in public_images.iterrows():
+        image_path = os.path.join(images_dir, row['image'])
+        image = imread(image_path)
+        if len(image.shape) != 3:
+            image = gray_to_color(image)
+        if resized:
+            image = imresize(image, (224, 224, 3))
+
+        image = np.expand_dims(image, axis=0) / 255
+        true_class = row['class'] - 1
         pred_class = np.argmax(model.predict(image)[0])
         if pred_class == true_class:
             correct += 1
 
     time_cons = time.time() - start
-    accuracy = correct / len(y_test)
+    accuracy = correct / public_images.shape[0]
     return accuracy, time_cons
 
 
